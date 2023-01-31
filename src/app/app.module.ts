350f96865594e36162c4c2773717d5d95946743e
andyanday33 <andyanday33@gmail.com>
2023-01-31 16:53:53 +0300

    feat: add navbar component, connected recipeBook components and shoppinList components to the app component, added a recipe model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
-import { RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipeBook/recipe-item/recipe-item.component';
+import { RecipesComponent } from './recipeBook/recipes.component';
 import { RecipeDetailComponent } from './recipeBook/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shoppingList/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipeBook/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
+    RecipesComponent,
     RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
   ],
   imports: [BrowserModule],
   providers: [],
