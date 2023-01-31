a3aef151638c10abaec9f09ef3adcfad55c4dd0f
andyanday33 <andyanday33@gmail.com>
2023-01-31 20:27:09 +0300

    feat(components): add draft of recipe list component

--- src/app/recipeBook/recipe-list/recipe-list.component.ts
+++ src/app/recipeBook/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe 1',
+      'Testing 1 2 1 2',
+      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'
+    ),
+    new Recipe(
+      'Test recipe 2',
+      'Testing 1 2 1 2',
+      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'
+    ),
+  ];
 }
