cc025c8abcc583a195c1c986e071882f346a2e83
andyanday33 <andyanday33@gmail.com>
2023-02-02 00:53:36 +0300

    feat: add ingredient model

--- src/app/shoppingList/shopping-list/shopping-list.component.ts
+++ src/app/shoppingList/shopping-list/shopping-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [];
 }
