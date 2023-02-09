668deee63ec3ae1eb3a0833a331d6a92e025a7ab
andyanday33 <andyanday33@gmail.com>
2023-02-09 18:36:46 +0300

    refactor(components): moved recipes to individual item components

--- src/app/recipeBook/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipeBook/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
 })
-export class RecipeItemComponent {}
+export class RecipeItemComponent implements OnInit, OnChanges {
+  @Input() recipe!: Recipe;
+
+  ngOnInit() {
+    this.checkRequiredFields(this.recipe);
+  }
+
+  ngOnChanges() {
+    this.checkRequiredFields(this.recipe);
+  }
+
+  checkRequiredFields(input: Recipe) {
+    if (input === null) {
+      throw new Error("Attribute 'recipe' is required");
+    }
+  }
+}
