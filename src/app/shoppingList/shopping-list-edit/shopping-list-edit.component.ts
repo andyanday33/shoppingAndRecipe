1bdcde94d79025b80ed135ff1fdd68a6a44b205c
andyanday33 <andyanday33@gmail.com>
2023-02-13 19:50:20 +0300

    feat(shopping list): Add new ingredients functionality

--- src/app/shoppingList/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shoppingList/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,7 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
 })
-export class ShoppingListEditComponent {}
+export class ShoppingListEditComponent {
+  @ViewChild('nameInput') nameInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('amountInput') amountInput!: ElementRef<HTMLInputElement>;
+
+  @Output() formSubmit: EventEmitter<Ingredient>;
+
+  constructor() {
+    this.formSubmit = new EventEmitter();
+  }
+
+  addIngredient() {
+    const name = this.nameInput.nativeElement.value;
+    const amount = this.amountInput.nativeElement.value;
+
+    if (name && amount)
+      this.formSubmit.emit({
+        name,
+        amount,
+      });
+  }
+}
